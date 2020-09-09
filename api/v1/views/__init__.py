a43eee528d7369f6d7b9bc856f0a6ecb42172e9d
Manga08 <bijuu5445@gmail.com>
2020-09-09 15:16:59 -0500

    [Featured]: The 4, 5, 6 task

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """Initialize app_views."""
 from flask import Blueprint
-from api.v1.views.index import *
-app_views = Blueprint('app_views', __name__, template_folder='templates')
+
+app_views = Blueprint('app_views', __name__, template_folder='templates', url_prefix="/api/v1")
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,13 +5,13 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/api/v1/status', strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status():
     """Return a JSON."""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def stats():
     """Endpoint that retrieves the number of each objects by type."""
     return jsonify({"amenities": storage.count('Amenity'),
