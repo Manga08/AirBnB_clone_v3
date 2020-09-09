379554ab96e410f51688cdbe189cfc5271a66918
Manga08 <bijuu5445@gmail.com>
2020-09-09 16:16:23 -0500

    [Featured]: The 4 Tasck

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """Start a Flask web application."""
 from flask import Flask, Blueprint, jsonify
 from models import storage
-from api.v1.views import app_views
+from api.v1.views.index import *
 from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
