8cf369a6e90ad804d42bc7d99653d540c3a204b4
Manga08 <bijuu5445@gmail.com>
2020-09-09 14:33:18 -0500

    [Featured]: The 4 task

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """create a route /status on the object app_views that returns a JSON."""
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 
@@ -8,15 +8,15 @@ from models import storage
 @app_views.route('/api/v1/status', strict_slashes=False)
 def status():
     """Return a JSON."""
-    return {"status": "OK"}
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/api/v1/stats', strict_slashes=False)
 def stats():
     """Endpoint that retrieves the number of each objects by type."""
-    return {"amenities": storage.count('Amenity'),
-            "cities": storage.count('City'),
-            "places": storage.count('Place'),
-            "reviews": storage.count('Review'),
-            "states": storage.count('State'),
-            "users": storage.count('User')}
+    return jsonify({"amenities": storage.count('Amenity'),
+                    "cities": storage.count('City'),
+                    "places": storage.count('Place'),
+                    "reviews": storage.count('Review'),
+                    "states": storage.count('State'),
+                    "users": storage.count('User')})
